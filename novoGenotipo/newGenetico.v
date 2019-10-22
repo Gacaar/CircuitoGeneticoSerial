842b66c50d9f1ce27116e4387e2b865205e2bbb6
Gabriel Arão <gabrielarao2013@gmail.com>
2019-10-22 11:43:30 -0300

    Calculando valores de s para cada iteração
    Como o verilog proíbe atualização de variável de síntese fora da sintaxe do for, é necessário inicializar os valores a cada iteração, precisando de umas equações um pouco complicadas.

--- novoGenotipo/newGenetico.v
+++ novoGenotipo/newGenetico.v
@@ -10,27 +10,68 @@ module newGenetico(saidas_LE, out_chrom, inp, out);
 	`include "parameters.sv"
 	
 	
-	genvar i, j, s;
+	genvar i, j;
 	generate
-
-	for (i = 0, s=0; i < ROW; i++)
+	for (i=0; i < ROW; i++)
 	begin : lcellsi
 		for(j = 0 ; j< COL; j++)
 		begin: lcellsj
+		// Determina o numero de ligacoes nos cantos e bordas (caso 1x1 nao necessario)
+		if(ROW==1 || COL==1)
+			begin
+				localparam integer c = 3;
+				localparam integer b = 2;
+			end
+		else
+			begin
+				localparam integer c = 2;
+				localparam integer b = 1;
+			end
+		// Determina o numero de entradas que ja puderam ser colocadas
+		if(i==0) //Primeira linha
+			begin
+				if(j==0) //Inicio linha
+					begin
+						localparam integer s = 0;
+					end
+				else //Meio ate fim da primeira linha
+					begin
+						localparam integer s = c+b*(j-1);
+					end
+			end
+		else
+			if(i<ROW-1) //Depois da primeira linha mas antes da ultima
+				begin
+					if(j==0) //Inicio da linha
+						begin
+							localparam integer s = (3-b)*c+COL-(3-b)+2*(i-1);
+						end
+					else //Meio a fim das linhas centrais. Nesse caso b=1 sempre
+						begin
+							localparam integer s = 2*c+COL-2+2*(i-1)+1;
+						end
+				end
+			else //Na ultima linha
+				begin
+					if(j==0) //Inicio da ultima linha
+						begin
+							localparam integer s = (3-b)*c+COL-(3-b)+2*(i-1);
+						end
+					else //Meio a fim da ultima linha. Nesse caso b=1 sempre
+						begin
+							localparam integer s = 2*c+COL-2+2*(i-1)+c*(j-1);
+						end
+				end
 			newlogic_e lcell( //NOVO LOGIC_E ???
 				.saidas(saidas_LE[i][j]),
 				.inp
 					({i==0?(s<IN?inp[s]:1'b0):LE_out[i-1][j]
-					, j==COL-1?((i==0? s+1:s)<IN?(i==0?(inp[s+1]:inp[s])):1'b0):LE_out[i][j+1]
+					, j==COL-1?((i==0? s+1:s)<IN?(i==0?inp[s+1]:inp[s]):1'b0):LE_out[i][j+1]
 					, i==ROW-1?((j==COL-1?(i==0?s+2:s+1):s)<IN?(j==COL-1?(i==0?inp[s+2]:inp[s+1]):inp[s]):1'b0):LE_out[i+1][j]
 					, j==0?((j==ROW-1?(j==COL-1?(i==0?s+3:s+2):s+1):s)<IN?(j==ROW-1?(j==COL-1?(i==0?inp[s+3]:inp[s+2]):inp[s+1]):inp[s]):1'b0):LE_out[i][j-1]
 					}),
 				.out(LE_out[i][j])
 			);
-			i==0?s=s+1
-			j==0?s=s+1
-			i==ROW-1?s=s+1
-			j==COL-1?s=s+1
 		end
 	end
 	endgenerate
